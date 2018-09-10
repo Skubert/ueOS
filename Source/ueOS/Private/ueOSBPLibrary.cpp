6a1038132c310c00dbc470884b7fff5ddb887b14
Piotr Skubała <skubert@gmail.com>
2018-09-10 15:44:56 +0200

    Added code to project, along with functions for listing files and reading files

---
+++ Source/ueOS/Private/ueOSBPLibrary.cpp
@@ -0,0 +1,44 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "ueOSBPLibrary.h"
+#include "ueOS.h"
+
+UueOSBPLibrary::UueOSBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+bool UueOSBPLibrary::FileSaveString(FString SaveTextB, FString FileNameB)
+{
+	return FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB));
+}
+
+bool UueOSBPLibrary::FileLoadString(FString FileNameA, FString& SaveTextA)
+{
+	return FFileHelper::LoadFileToString(SaveTextA, *(FPaths::GameDir() + FileNameA));
+}
+
+bool UueOSBPLibrary::GetFiles(TArray<FString>& Files, FString RootFolderFullPath, FString Ext)
+{
+	if (RootFolderFullPath.Len() < 1) return false;
+
+	FPaths::NormalizeDirectoryName(RootFolderFullPath);
+
+	IFileManager& FileManager = IFileManager::Get();
+
+	if (Ext == "")
+	{
+		Ext = "*.*";
+	}
+	else
+	{
+		Ext = (Ext.Left(1) == ".") ? "*" + Ext : "*." + Ext;
+	}
+
+	FString FinalPath = RootFolderFullPath + "/" + Ext;
+	FileManager.FindFiles(Files, *FinalPath, true, false);
+	return true;
+}
+
+
